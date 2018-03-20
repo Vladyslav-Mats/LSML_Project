787f304976ad463da736d2a4b3bba7a68325c3e6
Vladyslav-Mats <vladyslav.mats@ring.com>
2018-03-20 22:13:54 +0200

    Added main with command line args

--- GradientBoosting.cpp
+++ GradientBoosting.cpp
@@ -1,8 +1,24 @@
 #include <vector>
+#include <string>
 #include "SolveTree.cpp"
 
 class GradientBoosting {
     std::vector<SolveTree> trees;
+    double learning_rate;
+    int tree_number;
+    int tree_size;
+    
+    
+public:
+    GradientBoosting(double learning_rate, int tree_number, int tree_size) {
+        this->learning_rate = learning_rate;
+        this->tree_number = tree_number;
+        this->tree_size = tree_size;
+    }
+    
+    GradientBoosting(std::string model_path) {
+        // restore saved model
+    }
     
     void fit(const Dataset& dataset) {}
     
@@ -16,6 +32,10 @@ class GradientBoosting {
             }
         }
         return predictions;
+    }
+    
+    void save_model(std::string save_path) {
+        // save model
     }
 };
 
